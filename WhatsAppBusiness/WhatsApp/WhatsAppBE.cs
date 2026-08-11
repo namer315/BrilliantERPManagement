469e9e439944da18bd88c18199a9280f17106ac5
Osama <numanosamawork@gmail.com>
2026-08-11 11:26:07 +0200

    Add WhatsAppBE class for WhatsApp Business API integration
    Introduced WhatsAppBE class with fields for account ID, phone number ID, and access token. Added PostAsync method to send authenticated POST requests to the WhatsApp Business API using HTTPService. Included necessary using directives.

---
+++ WhatsAppBusiness/WhatsApp/WhatsAppBE.cs
@@ -0,0 +1,29 @@
+﻿using CommonData.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WhatsAppBusiness.WhatsApp;
+
+public class WhatsAppBE
+{
+    /// <summary>
+    /// {WHATSAPP_BUSINESS_ACCOUNT_ID} or {WABA_ID}
+    /// </summary>
+    private string _WhatsAppBusinessAccountId = "1550103273123935";
+    private readonly string _phoneNumberId = "1124475747418242";
+    private readonly string _accessToken = "EAARC64ekrboBRvMRrQ2gF3MZC1hopZCdrPoHiptdpr9TTq10ugRMX9ZAgzzolhs3kSqdAIAHojZA6Pog3lkwxHjKKmEkNZCLbWAzHpoJCH0QFvlttYPZCUeJVRuTygGdi3NgTJcTvxzX4JRkL3iBsCr5A0QbUZAaPrG7EvCayZBZA2X1znRe54pQsSZAhz7ZBB4nwZDZD";
+
+    private readonly static HTTPService _HTTPService = new HTTPService();
+
+    public async Task<string> PostAsync(string subURL , string jsonPayload)
+    {
+        string url = $"https://graph.facebook.com/v22.0/{_phoneNumberId}/{subURL}";
+
+        return await _HTTPService.PostAsync(url , jsonPayload , new AuthenticationHeaderValue("Bearer" , _accessToken));
+    }
+
+
+
+}
