a4ee2af079958078f089e58df080b2caa5a5af40
Osama <numanosamawork@gmail.com>
2026-08-15 14:24:34 +0200

    Add StreamDTO and StreamMessageDTO for stream data
    Introduced StreamDTO and StreamMessageDTO classes in the WhatsAppData.DTO.Stream namespace to represent and transfer stream message data. StreamDTO includes Token (conditionally ignored outside debug), TenentName, and a StreamMessageDTO Message. StreamMessageDTO provides From, Content, and DateTimeUTC properties.

---
+++ WhatsAppData/DTO/Stream/StreamDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WhatsAppData.DTO.Stream;
+
+public class StreamDTO
+{
+#if !DEBUG
+    [JsonIgnore]
+#endif
+    public string Token { get; set; }
+
+    public string TenentName { get; set; }
+
+    public StreamMessageDTO Message { get; set; }
+}
---
+++ WhatsAppData/DTO/Stream/StreamMessageDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatsAppData.DTO.Stream;
+
+public class StreamMessageDTO
+{
+    public string From { get; set; }
+    public string Content { get; set; }
+    public DateTimeOffset DateTimeUTC { get; set; }
+}
