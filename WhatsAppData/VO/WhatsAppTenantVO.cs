15af97f118ed7b54b6b42c1c77583c611392f5ab
Osama <numanosamawork@gmail.com>
2026-08-19 10:47:26 +0200

    Add WhatsAppTenantBE and WhatsAppTenantVO classes
    Introduced WhatsAppTenantBE as an empty class in WhatsAppBusiness. Added WhatsAppTenantVO with Contact and Tenant properties, inheriting from EntityBase. Included commented-out ORM mapping for future reference.

---
+++ WhatsAppData/VO/WhatsAppTenantVO.cs
@@ -0,0 +1,18 @@
+﻿using CommonData.VO;
+using WhatsAppData.VO.WhatsApp;
+
+namespace WhatsAppData.VO;
+
+public class WhatsAppTenantVO : EntityBase
+{
+    public virtual ContactVO Contact { get; set; }
+    public virtual TenantVO Tenant { get; set; }
+}
+
+//public class TenantMap : EntityWithCreatedAtMapping<WhatsAppTenantVO>
+//{
+//    public TenantMap()
+//    {
+//        References(x => x.Contact).Cascade.Merge();
+//    }
+//}
